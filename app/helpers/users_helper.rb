8101f040614523e2fc256c1a3107026f3c6290c5
Thomas Shipley <thomas.shipley@googlemail.com>
2015-07-18 18:17:32 +0100

    Half way through lesson - now have error handling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns a gravatar for a user
+  def gravatar_for(user)
+    hashed_email = Digest::MD5::hexdigest user.email.downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{hashed_email}"
+    image_tag gravatar_url, alt: user.name, class: 'gravatar'
+  end
 end
